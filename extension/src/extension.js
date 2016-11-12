a2b1b33fdd922943f0a3fbfd221025634f6ec826
kburk1997 <kburk97@optonline.net>
2016-11-12 14:23:17 -0500

    get stream

---
+++ extension/src/extension.js
@@ -0,0 +1,17 @@
+function process(element){
+	console.log(element);
+}
+
+
+/*ready('.stream', function (element) {
+  process(element);
+});*/
+
+
+$(document).ready(function(){
+	console.log("You are on Twitter!");
+	var stream=$(".stream-items");
+	console.log(stream);
+
+	
+});
